957f76985d1208816298b68a28a99af340e676ab
Pau Ramos <p.ramos@unistra.fr>
2020-10-22 16:27:14 +0200

    Trying to fix problems with the naming
    The instalation still works with the old name (sagittarius_properties) instead of the new (interpolators)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sagittarius-properties-brugalada", # Replace with your own username
+    name="sagittarius-brugalada", # Replace with your own username
     version="0.0.1",
     author="Pau Ramos",
     author_email="p.ramos@unistra.fr",
